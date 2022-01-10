41f81a676625c569fd0ae03180a91b64cd09a40d
woodendoors7 <60623935+woodendoors7@users.noreply.github.com>
2022-01-10 17:23:07 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+/*window.addEventListener('scroll', reveal);
+
+function reveal() {
+    let reveals = document.querySelectorAll('.reveal');
+
+    for (let i = 0; i < reveals.length; i++) {
+
+        let windowheight = window.innerHeight;
+        let revealTop = reveals[1].getBoundingClientRect().top;
+        let revealPoint = 0;
+
+        if (revealTop < windowheight - revealPoint) {
+            reveals[i].classList.add('active');
+        } else {
+            reveals[i].classList.remove('active')
+        }
+    }
+}*/
